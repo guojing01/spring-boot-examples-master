084d164ccd234e5bb75db9bf0ab7bb2f176ca0ea
renchunbao <renchunbao_01@163.com>
2018-04-26 15:23:02 +0800

    添加了druid连接池

--- spring-boot-shiro/src/main/java/com/neo/SpringBootShiroApplication.java
+++ spring-boot-shiro/src/main/java/com/neo/SpringBootShiroApplication.java
@@ -1,9 +1,12 @@
 package com.neo;
 
+import de.codecentric.boot.admin.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
+//@EnableAdminServer
 public class SpringBootShiroApplication {
 
 	public static void main(String[] args) {
