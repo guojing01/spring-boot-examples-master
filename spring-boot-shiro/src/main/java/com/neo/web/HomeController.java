9c25485011459a6ead7b35459fb6b294acf39b26
renchunbao <renchunbao_01@163.com>
2018-05-15 15:43:15 +0800

    添加退出登录操作

--- spring-boot-shiro/src/main/java/com/neo/web/HomeController.java
+++ spring-boot-shiro/src/main/java/com/neo/web/HomeController.java
@@ -11,9 +11,13 @@ import org.apache.shiro.session.Session;
 import org.apache.shiro.subject.Subject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
 import java.util.Map;
@@ -21,12 +25,14 @@ import java.util.Map;
 @Controller
 public class HomeController extends BaseController{
     private final static Logger log = LoggerFactory.getLogger(RedisUtil.class);
+    @Autowired
+    private RedisUtil redisUtil;
 
     @RequestMapping("/login")
     @ResponseBody
     public Object login(HttpServletRequest request, String username, String password) throws Exception{
         log.info("测试日志是否进来，登陆开始执行了。。。。。。。。。。。");
-        Map<String, Object> jsonObject = new HashMap();
+        Map<String, Object> map = new HashMap();
         Subject currentUser = SecurityUtils.getSubject();
 
         try {
@@ -53,25 +59,25 @@ public class HomeController extends BaseController{
                 Session session = currentUser.getSession();
                 session.setAttribute("currentUserInfo", userinfo);
                 session.setAttribute("currentUserID", userinfo.getUid());
-                session.setTimeout(1800000L);//毫秒
-                jsonObject.put("token", currentUser.getSession().getId());
-                jsonObject.put("msg", "登录成功");
+                session.setTimeout(1800000L);//毫秒   30分钟
+                map.put("token", currentUser.getSession().getId());
+                map.put("msg", "登录成功");
             }
         } catch (UnknownAccountException var9) {
             var9.printStackTrace();
-            jsonObject.put("msg", "账户不存在！");
+            map.put("msg", "账户不存在！");
         } catch (IncorrectCredentialsException var10) {
             var10.printStackTrace();
-            jsonObject.put("msg", "密码不正确！");
+            map.put("msg", "密码不正确！");
         } catch (LockedAccountException var11) {
             var11.printStackTrace();
-            jsonObject.put("msg", "账户已锁定！");
+            map.put("msg", "账户已锁定！");
         } catch (Exception var12) {
             var12.printStackTrace();
-            jsonObject.put("msg", "用户名或者密码错误！");
+            map.put("msg", "用户名或者密码错误！");
         }
 
-        return jsonObject.toString();
+        return map.toString();
     }
 
     @RequestMapping({"/unauth"})
@@ -89,6 +95,28 @@ public class HomeController extends BaseController{
         Map<String, Object> map = new HashMap();
         map.put("code", "1000000");
         map.put("msg", "未登录");
+        return map;
+    }
+
+
+    @RequestMapping("/logout")
+    @ResponseBody
+    public Object logout() throws Exception {
+        Map<String, Object> map = new HashMap();
+        //在这里执行退出系统前需要清空的数据
+        try {
+            UserInfo userinfo = getCurrentUser();
+            Subject subject = SecurityUtils.getSubject();
+            if (subject.isAuthenticated()) {
+                subject.logout(); // session 会销毁，在SessionListener监听session销毁，清理权限缓存
+            }
+            map.put("code", "1000002");
+            map.put("msg", userinfo.getUsername() + "退出登录成功！");
+            return map;
+        } catch (Exception e) {
+            log.info("退出登录失败！");
+        }
+        map.put("msg", "退出登录失败！");
         return map;
     }
 }
